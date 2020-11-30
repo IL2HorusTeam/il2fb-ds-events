77a8f0a2254b2443d7074cc43db2aebc6e90bd16
Oleksandr Oblovatnyi <oblovatniy@gmail.com>
2020-11-30 16:53:01 +0200

    Add 'DatetimeMixin'

--- il2fb/ds/events/definitions/mixins.py
+++ il2fb/ds/events/definitions/mixins.py
@@ -4,5 +4,10 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
+class DatetimeMixin:
+  datetime: datetime.datetime
+
+
+@dataclass(frozen=True)
 class TimeMixin:
   time: datetime.time
