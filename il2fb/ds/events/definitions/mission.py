5da0164a143153ba6e39376d3fab77e3f8a62efa
Oleksandr Oblovatnyi <oblovatniy@gmail.com>
2020-12-01 16:16:42 +0200

    Define mission status events

---
+++ il2fb/ds/events/definitions/mission.py
@@ -0,0 +1,79 @@
+import sys
+
+if sys.version_info >= (3, 9):
+  Dict = dict
+else:
+  from typing import Dict
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from typing import Any
+
+from il2fb.commons.structures import PrimitiveDataclassMixin
+
+from .base import Event
+
+from .mixins import DatetimeMixin
+from .mixins import TimeMixin
+
+from .registry import register
+
+from ._utils import export
+from ._translations import gettext_lazy as _
+
+
+@export
+@dataclass(frozen=True)
+class MissionLoadedInfo(DatetimeMixin, PrimitiveDataclassMixin):
+  __slots__ = ["datetime", "file_path", ]
+
+  file_path: Path
+
+  def to_primitive(self, *args, **kwargs) -> Dict[str, Any]:
+    return {
+      "datetime": self.datetime.isoformat(),
+      "file_path": str(self.file_path),
+    }
+
+
+@export
+@dataclass(frozen=True)
+class MissionStartedInfo(TimeMixin, PrimitiveDataclassMixin):
+  __slots__ = ["time", ]
+
+
+@export
+@dataclass(frozen=True)
+class MissionEndedInfo(TimeMixin, PrimitiveDataclassMixin):
+  __slots__ = ["time", ]
+
+
+@export
+@dataclass(frozen=True)
+class MissionStatusEvent(Event):
+  category = "mission"
+
+
+@export
+@register
+@dataclass(frozen=True)
+class MissionLoadedEvent(MissionStatusEvent):
+  verbose_name = _("Mission loaded")
+  data: MissionLoadedInfo
+
+
+@export
+@register
+@dataclass(frozen=True)
+class MissionStartedEvent(MissionStatusEvent):
+  verbose_name = _("Mission started")
+  data: MissionStartedInfo
+
+
+@export
+@register
+@dataclass(frozen=True)
+class MissionEndedEvent(MissionStatusEvent):
+  verbose_name = _("Mission ended")
+  data: MissionEndedInfo
---
+++ tests/test_mission.py
@@ -0,0 +1,127 @@
+import datetime
+import unittest
+
+from pathlib import Path
+
+from il2fb.ds.events.definitions.base import Event
+
+from il2fb.ds.events.definitions.mission import MissionLoadedInfo
+from il2fb.ds.events.definitions.mission import MissionStartedInfo
+from il2fb.ds.events.definitions.mission import MissionEndedInfo
+
+from il2fb.ds.events.definitions.mission import MissionStatusEvent
+from il2fb.ds.events.definitions.mission import MissionLoadedEvent
+from il2fb.ds.events.definitions.mission import MissionStartedEvent
+from il2fb.ds.events.definitions.mission import MissionEndedEvent
+
+from il2fb.ds.events.definitions import registry
+
+
+class MissionStatusEventTestCase(unittest.TestCase):
+
+  def test_derives_from_Event(self):
+    self.assertTrue(issubclass(MissionStatusEvent, Event))
+
+
+class MissionLoadedEventTestCase(unittest.TestCase):
+
+  def test_derives_from_MissionStatusEvent(self):
+    self.assertTrue(issubclass(MissionLoadedEvent, MissionStatusEvent))
+
+  def test_is_registered(self):
+    self.assertEqual(
+      registry.get_class_by_name("MissionLoadedEvent"),
+      MissionLoadedEvent,
+    )
+
+  def test_to_primitive(self):
+    testee = MissionLoadedEvent(
+      MissionLoadedInfo(
+        datetime=datetime.datetime(2020, 8, 3, 15, 26, 45),
+        file_path=Path('net/dogfight/1596469535.mis'),
+      ),
+    )
+    self.assertEqual(testee.to_primitive(), {
+      'category': 'mission',
+      'name': 'MissionLoadedEvent',
+      'verbose_name': 'Mission loaded',
+      'help_text': None,
+      'data': {
+        'datetime': '2020-08-03T15:26:45',
+        'file_path': 'net/dogfight/1596469535.mis',
+      },
+    })
+
+  def test_from_primitive(self):
+    testee = MissionLoadedEvent(
+      MissionLoadedInfo(
+        datetime=datetime.datetime(2020, 8, 3, 15, 26, 45),
+        file_path=Path('net/dogfight/1596469535.mis'),
+      ),
+    )
+    self.assertEqual(
+      testee,
+      MissionLoadedEvent.from_primitive(testee.to_primitive()),
+    )
+
+
+class MissionStartedEventTestCase(unittest.TestCase):
+
+  def test_derives_from_MissionStatusEvent(self):
+    self.assertTrue(issubclass(MissionStartedEvent, MissionStatusEvent))
+
+  def test_is_registered(self):
+    self.assertEqual(
+      registry.get_class_by_name("MissionStartedEvent"),
+      MissionStartedEvent,
+    )
+
+  def test_to_primitive(self):
+    testee = MissionStartedEvent(MissionStartedInfo(datetime.time(15, 26, 45)))
+    self.assertEqual(testee.to_primitive(), {
+      'category': 'mission',
+      'name': 'MissionStartedEvent',
+      'verbose_name': 'Mission started',
+      'help_text': None,
+      'data': {
+        'time': '15:26:45',
+      },
+    })
+
+  def test_from_primitive(self):
+    testee = MissionStartedEvent(MissionStartedInfo(datetime.time(15, 26, 45)))
+    self.assertEqual(
+      testee,
+      MissionStartedEvent.from_primitive(testee.to_primitive()),
+    )
+
+
+class MissionEndedEventTestCase(unittest.TestCase):
+
+  def test_derives_from_MissionStatusEvent(self):
+    self.assertTrue(issubclass(MissionEndedEvent, MissionStatusEvent))
+
+  def test_is_registered(self):
+    self.assertEqual(
+      registry.get_class_by_name("MissionEndedEvent"),
+      MissionEndedEvent,
+    )
+
+  def test_to_primitive(self):
+    testee = MissionEndedEvent(MissionEndedInfo(datetime.time(15, 26, 45)))
+    self.assertEqual(testee.to_primitive(), {
+      'category': 'mission',
+      'name': 'MissionEndedEvent',
+      'verbose_name': 'Mission ended',
+      'help_text': None,
+      'data': {
+        'time': '15:26:45',
+      },
+    })
+
+  def test_from_primitive(self):
+    testee = MissionEndedEvent(MissionEndedInfo(datetime.time(15, 26, 45)))
+    self.assertEqual(
+      testee,
+      MissionEndedEvent.from_primitive(testee.to_primitive()),
+    )
